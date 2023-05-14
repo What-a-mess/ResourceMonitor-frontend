ddcc2e44e0aebf2ddea0a99c6dcbee1e63fabfc5
wamess <44860987+What-a-mess@users.noreply.github.com>
2023-05-14 21:11:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <el-container class="thecontainer">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="1"
+        >
+          <el-menu-item index="1">Option 1</el-menu-item>
+          <el-menu-item index="2">Option 2</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <div id="content"><router-view/></div>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+  #app {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+  }
+  #content {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import {createRouter, createWebHistory, createWebHashHistory} from 'vue-router'
+
+import Home from './components/Home.vue'
+
+const routes = [
+    {
+    path: '/home',
+    component: Home
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
